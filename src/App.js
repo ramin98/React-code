06fb5f5a55a0e47ae1074c6b77b7638aa372ac37
ramin98 <59912261+ramin98@users.noreply.github.com>
2023-09-15 16:19:32 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer footerText='Footer'/>
+    </div>
+  );
+}
+
+
+
+export default App;
